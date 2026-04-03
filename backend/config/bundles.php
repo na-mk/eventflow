36dcbeaede4e5baee11b351122b52e3625eca9cf
Anna Mk07 <annakaya.mk@gmail.com>
2026-04-03 17:46:08 +0200

    Guard MakerBundle loading in all environments

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -1,11 +1,18 @@
 <?php
 
-return [
+use Symfony\Bundle\MakerBundle\MakerBundle;
+
+$bundles = [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
+
+if (class_exists(MakerBundle::class)) {
+    $bundles[MakerBundle::class] = ['dev' => true];
+}
+
+return $bundles;
